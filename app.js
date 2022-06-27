e078b5fc9500c59564857d27d485940b6195a303
Effi Phil <effiphil@Effis-MacBook-Pro-2.local>
2022-06-27 18:07:29 +0300

    added ip print

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const ip = require("ip");
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => res.send("your local ip is: " + ip.address()))
 app.listen(PORT, (error) =>{
     if(!error)
-        console.log(ip.address())
+        console.log("Server running on port + ",PORT )
     else 
         console.log("Error occurred, server can't start", error);
     }
