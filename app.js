31af6c6c9eab845130e66278d150d6e473d8d215
effi24 <107849233+effi24@users.noreply.github.com>
2022-06-30 14:51:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 app.get('/', (req, res) => res.send("your local ip is: " + ip.address()))
 app.listen(PORT, (error) =>{
     if(!error)
-        console.log("Server running on port  ",PORT )
+        console.log("Server running on port ",PORT )
     else 
         console.log("Error occurred, server can't start", error);
     }
